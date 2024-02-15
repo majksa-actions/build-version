5be20947ec6dde57319b461f3cfebeb30fbd1b0d
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-02-15 17:33:48 +0100

    feat: setup github actions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello to GitHub actions via Bun!');
+import * as core from '@actions/core';
+
+/**
+ * GitHub Action entrypoint.
+ */
+async function run(): Promise<void> {
+  try {
+    core.debug('Hello to GitHub actions via Bun!');
+    core.setOutput('date', new Date().toDateString());
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+    else core.setFailed(String(error));
+  }
+}
+
+run();
