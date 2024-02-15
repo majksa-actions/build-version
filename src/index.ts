4d8503986d996b6fe6fee9c9cb4fdbc2ba1b7ca6
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-02-15 19:13:58 +0100

    feat: add date notice to output

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ import * as core from '@actions/core';
 async function run(): Promise<void> {
   try {
     core.debug('Hello to GitHub actions via Bun!');
-    core.setOutput('date', new Date().toDateString());
+    const date = new Date().toDateString();
+    core.setOutput('date', date);
+    core.notice(date, { title: 'Date' });
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
     else core.setFailed(String(error));
